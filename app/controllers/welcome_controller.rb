27ad2f332691f7dfd3fd3e8630993750f8599b06
bagimene <bagimene@uc.cl>
2016-05-15 12:41:58 -0400

    Avanzado.v7

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,9 @@ class WelcomeController < ApplicationController
   #include
   def index
   	#@popular = Instagram.media_popular
-  	#@show = versionCommit
+  	@show = versionCommit
   	#@show = httpGetRequest("https://api.instagram.com/v1/tags/snowy/media/recent?access_token=2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402" , nil )
-  	@show = httpPostRequest(rutaPost, nil, {"tag"=> "snowy", "extra"=> "de más", "access_token"=> access_token} )
+  	#@show = httpPostRequest(rutaPost, nil, {"tag"=> "snowy", "extra"=> "de más", "access_token"=> access_token} )
     #@show = convertirStringInt("versiónSinNúmero437")
   end
 end
